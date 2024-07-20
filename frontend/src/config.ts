8125c131c0e8d893ca251f1e0299a60ee75d8667
SwatejReddy <109172700+SwatejReddy@users.noreply.github.com>
2024-07-20 13:16:13 +0530

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'https://medium.swatejreddy17.workers.dev'
+// export const BACKEND_URL = 'https://medium.swatejreddy17.workers.dev'
+export const BACKEND_URL = 'https://backend.swatejreddy17.workers.dev/'
